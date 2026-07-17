37cd165dc96f13e808df186980d95a6242f6c99e
Mr Ole <72989100+MrOlee@users.noreply.github.com>
2026-07-18 01:48:41 +0700

    Create initial App component with basic layout

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+    return (
+        <div style={{ textAlign: 'center', marginTop: '50px', fontFamily: 'sans-serif' }}>
+            <h1>Rayliziie Media Digital</h1>
+            <p>Sistem sedang dipersiapkan.</p>
+        </div>
+    );
+};
+
+export default App;
