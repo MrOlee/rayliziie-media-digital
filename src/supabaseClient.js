c40b2244342330371f28c49a14f7bfd8faae46a9
Mr Ole <72989100+MrOlee@users.noreply.github.com>
2026-07-18 21:55:01 +0700

    Initialize Supabase client with URL and key

---
+++ src/supabaseClient.js
@@ -0,0 +1,7 @@
+import { createClient } from '@supabase/supabase-js';
+
+// GANTI DENGAN URL & KEY SUPABASE LU
+const supabaseUrl = 'https://harpdcqmrqdgckcuhxfr.supabase.co';
+const supabaseKey = 'sb_publishable_ppzSXi7DuN7v0racT9l98A_JxK5-MGG';
+
+export const supabase = createClient(supabaseUrl, supabaseKey);
